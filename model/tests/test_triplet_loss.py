641472f3ee649ae95463e6018e1234f496c651bd
Yuol96 <huangyc96@gmail.com>
2018-09-05 16:57:50 +0800

    finish batch all strategy

--- model/tests/test_triplet_loss.py
+++ model/tests/test_triplet_loss.py
@@ -2,6 +2,8 @@ import numpy as np
 import torch
 
 from model.triplet_loss import pairwise_distances as pairwise_distances_pytorch
+from model.triplet_loss import get_valid_triplets_mask
+from model.triplet_loss import batch_all_triplet_loss
 
 def pairwise_distance_np(feature, squared=False):
     """Computes the pairwise distance matrix in numpy.
@@ -37,3 +39,67 @@ def test_pairwise_distances():
     	res_np = pairwise_distance_np(embeddings, squared=squared)
     	res_pytorch = pairwise_distances_pytorch(torch.from_numpy(embeddings), squared=squared)
     	assert np.allclose(res_np, res_pytorch.numpy())
+
+def test_triplet_mask():
+    """Test function _get_triplet_mask."""
+    num_data = 64
+    num_classes = 10
+
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    mask_np = np.zeros((num_data, num_data, num_data))
+    for i in range(num_data):
+        for j in range(num_data):
+            for k in range(num_data):
+                distinct = (i != j and i != k and j != k)
+                valid = (labels[i] == labels[j]) and (labels[i] != labels[k])
+                mask_np[i, j, k] = (distinct and valid)
+
+    mask_pytorch = get_valid_triplets_mask(torch.from_numpy(labels)).numpy()
+
+    assert np.allclose(mask_np, mask_pytorch)
+
+def test_batch_all_triplet_loss():
+    """Test the triplet loss with batch all triplet mining"""
+    num_data = 10
+    feat_dim = 6
+    margin = 0.2
+    num_classes = 5
+
+    embeddings = np.random.rand(num_data, feat_dim).astype(np.float32)
+    labels = np.random.randint(0, num_classes, size=(num_data)).astype(np.float32)
+
+    for squared in [True, False]:
+        pdist_matrix = pairwise_distance_np(embeddings, squared=squared)
+
+        loss_np = 0.0
+        num_positives = 0.0
+        num_valid = 0.0
+        for i in range(num_data):
+            for j in range(num_data):
+                for k in range(num_data):
+                    distinct = (i != j and i != k and j != k)
+                    valid = (labels[i] == labels[j]) and (labels[i] != labels[k])
+                    if distinct and valid:
+                        num_valid += 1.0
+
+                        pos_distance = pdist_matrix[i][j]
+                        neg_distance = pdist_matrix[i][k]
+
+                        loss = np.maximum(0.0, pos_distance - neg_distance + margin)
+                        loss_np += loss
+
+                        num_positives += (loss > 0)
+
+        loss_np /= num_positives
+
+        # Compute the loss in TF.
+        loss_pytorch, fraction = batch_all_triplet_loss(torch.from_numpy(labels), torch.from_numpy(embeddings), margin, squared=squared)
+
+        assert np.allclose(loss_np, loss_pytorch.item())
+        assert np.allclose(num_positives / num_valid, fraction.item())
+
+
+
+
+
--- model/triplet_loss.py
+++ model/triplet_loss.py
@@ -23,4 +23,49 @@ def pairwise_distances(embeddings, squared=False):
 
 	return distances
 
-# def batch_all_triplet_loss
+def get_valid_triplets_mask(labels):
+	"""
+	To be valid, a triplet (a,p,n) has to satisfy:
+		- a,p,n are distinct embeddings
+		- a and p have the same label, while a and n have different label
+	"""
+	indices_equal = torch.eye(labels.size(0)).byte()
+	indices_not_equal = ~indices_equal
+	i_ne_j = indices_not_equal.unsqueeze(2)
+	i_ne_k = indices_not_equal.unsqueeze(1)
+	j_ne_k = indices_not_equal.unsqueeze(0)
+	distinct_indices = i_ne_j & i_ne_k & j_ne_k
+
+	label_equal = torch.eq(labels.unsqueeze(1), labels.unsqueeze(0))
+	i_eq_j = label_equal.unsqueeze(2)
+	i_eq_k = label_equal.unsqueeze(1)
+	i_ne_k = ~i_eq_k
+	valid_labels = i_eq_j & i_ne_k
+
+	mask = distinct_indices & valid_labels
+	return mask
+
+def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
+
+	distances = pairwise_distances(embeddings, squared=squared)
+
+	anchor_positive_dist = distances.unsqueeze(2)
+	anchor_negative_dist = distances.unsqueeze(1)
+	triplet_loss = anchor_positive_dist - anchor_negative_dist + margin
+
+	# get a 3D mask to filter out invalid triplets
+	mask = get_valid_triplets_mask(labels)
+
+	triplet_loss = triplet_loss * mask.float()
+	triplet_loss.clamp_(min=0)
+
+	# count the number of positive triplets
+	epsilon = 1e-16
+	num_positive_triplets = (triplet_loss > 0).float().sum()
+	num_valid_triplets = mask.float().sum()
+	fraction_positive_triplets = num_positive_triplets / (num_valid_triplets + epsilon)
+
+	triplet_loss = triplet_loss.sum() / (num_positive_triplets + epsilon)
+
+	return triplet_loss, fraction_positive_triplets
+
